1e197460f9d8ea0a74620d8219fb8e206219fb1e
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-28 23:16:13 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Bruce Gilmour on 2021-07-28.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
