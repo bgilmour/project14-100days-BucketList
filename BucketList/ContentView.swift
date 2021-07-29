18d869b9f361fcb3fc6505c3ca5454349de6635e
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-29 01:26:34 +0100

    Switching view states with enums

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -9,10 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        DocumentsTestView()
+        LoadingStateEnumTestView()
     }
 }
 
+struct LoadingStateEnumTestView: View {
+    var loadingState = LoadingState.loading
+
+    var body: some View {
+        Group {
+            if loadingState == .loading {
+                LoadingView()
+            } else if loadingState == .success {
+                SuccessView()
+            } else if loadingState == .failed {
+                FailedView()
+            }
+        }
+    }
+}
+
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
 struct DocumentsTestView: View {
 
     var body: some View {
